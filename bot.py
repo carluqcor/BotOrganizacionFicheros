d2a985900cccc8a1340d46f8550f87c2fdd29bf8
Carlos Luque Córdoba <i62lucoc@uco.es>
2018-10-27 17:27:31 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,28 +1,16 @@
 import telebot
-import json
 import os
-
-token = os.environ['TELEGRAM_TOKEN']
-bot = telebot.TeleBot(token)
-
+import json
 
 with open('usuarios.json') as f:
     usuarios = json.load(f)
 
 def es_usuario(id_usuario):
-    return str(id_usuario) in list(usuarios.keys())
+    return any(usuarios.get(str(id_usuario)))
 
-def add_usuario(id_usuario):
-    usuarios[str(id_usuario)]=[]
-    with open('usuarios.json', "w") as f:
-        json.dump(usuarios, f, indent=2)
+token = os.environ['TELEGRAM_TOKEN']
 
-def delete_user(id_usuario):
-    del(usuarios[str(id_usuario)])
-    with open('usuarios.json', "w") as f:
-        json.dump(usuarios, f, indent=2)
-
-# token = '669166473:AAGRjddRvsm23At_fTRKNdhv4y2wFCthjdU'
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands = ['start'])
 def handle_start(m):
@@ -32,16 +20,6 @@ def handle_start(m):
         bot.send_message(cid, "Bienvenido al bot!")
     else:
         bot.send_message(cid, "Ya eres usuario")
-
-@bot.message_handler(commands = ['stop'])
-def handle_stop(m):
-    cid = m.chat.id
-    if es_usuario(cid):
-        delete_user(cid)
-        bot.send_message(cid, "Usuario eliminado")
-    else:
-        bot.send_message(cid, "El usuario no se ha podido eliminar por que no existe")
-
 
 
 bot.polling()
