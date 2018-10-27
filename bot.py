57786d836ce5fba4b6ba3ce2d80e0eba05631bba
Eduardo Roldan <i32ropie@uco.es>
2018-10-27 11:55:11 +0200

    Cambiada función es_usuario

--- bot.py
+++ bot.py
@@ -6,7 +6,10 @@ with open('usuarios.json') as f:
     usuarios = json.load(f)
 
 def es_usuario(id_usuario):
-    return any(usuarios.get(str(id_usuario)))
+    return str(id_usuario) in list(usuarios.keys())
+
+def add_usuario(id_usuario):
+    usuarios[str(id_usuario)] = []
 
 token = os.environ['TELEGRAM_TOKEN']
 
@@ -16,7 +19,7 @@ bot = telebot.TeleBot(token)
 def handle_start(m):
     cid = m.chat.id
     if not es_usuario(cid):
-        add_usuario(cid)
+        # add_usuario(cid)
         bot.send_message(cid, "Bienvenido al bot!")
     else:
         bot.send_message(cid, "Ya eres usuario")
