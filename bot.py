dd6b94739b4245f30540802e4359d09870aeabe8
Eduardo Roldan <i32ropie@uco.es>
2018-10-27 11:36:41 +0200

    Añadida parte del código

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
 import telebot
 import os
+import json
+
+with open('usuarios.json') as f:
+    usuarios = json.load(f)
+
+def es_usuario(id_usuario):
+    return any(usuarios.get(str(id_usuario)))
 
 token = os.environ['TELEGRAM_TOKEN']
 
@@ -7,6 +14,12 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands = ['start'])
 def handle_start(m):
-    bot.send_message(m.chat.id, "Hola")
+    cid = m.chat.id
+    if not es_usuario(cid):
+        add_usuario(cid)
+        bot.send_message(cid, "Bienvenido al bot!")
+    else:
+        bot.send_message(cid, "Ya eres usuario")
+
 
 bot.polling()
