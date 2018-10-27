dc2a093a7a31f32bf4d8ab7f0d8d10f53c681999
i62lucoc <i62lucoc@uco.es>
2018-10-27 11:56:46 +0200

    Se añade funcion usuario y comprobar si existe

---
+++ MisCosas.py
@@ -0,0 +1,4 @@
+token = "669166473:AAGRjddRvsm23At_fTRKNdhv4y2wFCthjdU"
+
+
+python3 bot.py
--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 import telebot
-import os
 import json
 
 with open('usuarios.json') as f:
     usuarios = json.load(f)
 
 def es_usuario(id_usuario):
-    return any(usuarios.get(str(id_usuario)))
+    return str(id_usuario) in list(usuarios.keys())
 
-token = os.environ['TELEGRAM_TOKEN']
+def add_usuario(id_usuario):
+    usuarios[str(id_usuario)]=[]
+    with open('usuarios.json', "w") as f:
+        json.dump(usuarios, f, indent=2)
+
+token = '669166473:AAGRjddRvsm23At_fTRKNdhv4y2wFCthjdU'
 
 bot = telebot.TeleBot(token)
 
